0218d75f6b4a07a0a44f95c402aa1bc41037f7da
sem laan <semlaan@dds.nl>
2023-04-08 18:28:28 +0200

    Made a sphere class that inherits from hitable

---
+++ raytracing/raytracing/RayMath/sphere.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "hitable.h"
+
+class Sphere : public Hitable {
+public:
+	vec3 center;
+	float radius;
+
+	Sphere() {}
+	Sphere(vec3 cen, float r) : center(cen), radius(r) {};
+	virtual bool Hit(const Ray& r, float t_min, float t_max, HitRecord& record) const;
+};
+
+bool Sphere::Hit(const Ray& r, float t_min, float t_max, HitRecord& record) const {
+    // ABC formula to determine whether the given ray intersects the given sphere
+    vec3 oc = r.Origin() - center;
+    float a = Dot(r.Direction(), r.Direction());
+    float b = Dot(oc, r.Direction());
+    float c = Dot(oc, oc) - radius * radius;
+    float discriminant = b * b - a * c;
+
+    // If there is a collision 
+    // calculate how far away from the origin of the ray the collision is
+    if (discriminant > 0) {
+        float temp = (-b - sqrt(discriminant)) / a;
+        if (temp > t_min && temp < t_max) {
+            record.t = temp;
+            record.p = r.PointAtParameter(record.t);
+            record.normal = (record.p - center) / radius;
+            return true;
+        }
+        temp = (-b + sqrt(discriminant)) / a;
+        if (temp > t_min && temp < t_max) {
+            record.t = temp;
+            record.p = r.PointAtParameter(record.t);
+            record.normal = (record.p - center) / radius;
+            return true;
+        }
+    }
+    return false;
+}
