7f232e28c0e0e0552fa30003f9bf673e1dc309b1
sem laan <semlaan@dds.nl>
2023-04-08 18:48:19 +0200

    Raytracing now uses the hitable classes

--- raytracing/raytracing/raytracing.cpp
+++ raytracing/raytracing/raytracing.cpp
@@ -1,34 +1,22 @@
 #include <iostream>
 #include <fstream>
-#include "RayMath/vec3.h"
-#include "RayMath/ray.h"
+#include "cfloat"
+#include <chrono>
+
+#include "RayMath/hitablelist.h"
+#include "RayMath/sphere.h"
 
 
-float HitSphere(const vec3& center, float radius, const Ray& r) {
-    // ABC formula to determine whether the given ray intersects the given sphere
-    vec3 oc = r.Origin() - center;
-    float a = Dot(r.Direction(), r.Direction());
-    float b = 2.0 * Dot(oc, r.Direction());
-    float c = Dot(oc, oc) - radius * radius;
-    float discriminant = b * b - 4 * a * c;
-
-    if (discriminant < 0) {
-        return -1.0f;
+vec3 Color(const Ray& r, Hitable* scene) {
+    HitRecord record;
+    if (scene->Hit(r, 0.0, FLT_MAX, record)) {
+        return 0.5 * vec3(record.normal.x() + 1, record.normal.y() + 1, record.normal.z() + 1);
     }
-    else { // If there is a collision calculate how far away from the origin of the ray the collision is
-        return (-b - sqrt(discriminant)) / (2.0 * a);
+    else {
+        vec3 unitDirection = UnitVector(r.Direction());
+        float t = 0.5f * (unitDirection.y() + 1.0f);
+        return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
     }
-}
-
-vec3 Color(const Ray& r) {
-    float t = HitSphere(vec3(0, 0, -1), 0.5f, r);
-    if (t > 0.0) {
-        vec3 N = UnitVector(r.PointAtParameter(t) - vec3(0, 0, -1));
-        return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
-    }
-    vec3 unitDirection = UnitVector(r.Direction());
-    t = 0.5f * (unitDirection.y() + 1.0f);
-    return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
 }
 
 
@@ -48,6 +36,15 @@ int main()
     vec3 vertical(0.0, 2.0, 0.0);
     vec3 origin(0.0, 0.0, 0.0);
 
+    // Defining the scene
+    const int objectCount = 2;
+    Hitable* list[objectCount];
+    list[0] = new Sphere(vec3(0, 0, -1), 0.5);
+    list[1] = new Sphere(vec3(0, -100.5, -1), 100);
+    Hitable* scene = new HitableList(list, objectCount);
+
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
     for (int j = ny - 1; j >= 0; j--)
     {
         for (int i = 0; i < nx; i++)
@@ -57,13 +54,17 @@ int main()
 
             Ray r(origin, lowerLeftCorner + u * horizontal + v * vertical);
 
-            vec3 color = Color(r);
+            vec3 color = Color(r, scene);
             int ir = int(255.99 * color[0]);
             int ig = int(255.99 * color[1]);
             int ib = int(255.99 * color[2]);
             imageFile << ir << " " << ig << " " << ib << "\n";
         }
     }
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[miliseconds]" << std::endl;
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::seconds> (end - begin).count() << "[seconds]" << std::endl;
 
     imageFile.close();
 }
