1a48450b09864bd85e981ce45777d2d75b5b9eb9
sem laan <semlaan@dds.nl>
2023-04-08 18:27:59 +0200

    main now renders a background and a sphere using raytracing

--- raytracing/raytracing/raytracing.cpp
+++ raytracing/raytracing/raytracing.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 #include <fstream>
-#include "vec3.h"
+#include "RayMath/vec3.h"
+#include "RayMath/ray.h"
+
+
+float HitSphere(const vec3& center, float radius, const Ray& r) {
+    // ABC formula to determine whether the given ray intersects the given sphere
+    vec3 oc = r.Origin() - center;
+    float a = Dot(r.Direction(), r.Direction());
+    float b = 2.0 * Dot(oc, r.Direction());
+    float c = Dot(oc, oc) - radius * radius;
+    float discriminant = b * b - 4 * a * c;
+
+    if (discriminant < 0) {
+        return -1.0f;
+    }
+    else { // If there is a collision calculate how far away from the origin of the ray the collision is
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
+}
+
+vec3 Color(const Ray& r) {
+    float t = HitSphere(vec3(0, 0, -1), 0.5f, r);
+    if (t > 0.0) {
+        vec3 N = UnitVector(r.PointAtParameter(t) - vec3(0, 0, -1));
+        return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+    vec3 unitDirection = UnitVector(r.Direction());
+    t = 0.5f * (unitDirection.y() + 1.0f);
+    return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
+}
 
 
 int main()
@@ -8,16 +37,27 @@ int main()
     int nx = 200;
     int ny = 100;
 
+    // Opening the image file and outputing the file settings into it
     std::ofstream imageFile;
     imageFile.open("Image.ppm");
-
     imageFile << "P3\n" << nx << " " << ny << "\n255\n";
+
+    // Defining the camera?
+    vec3 lowerLeftCorner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
 
     for (int j = ny - 1; j >= 0; j--)
     {
         for (int i = 0; i < nx; i++)
         {
-            vec3 color(float(i) / float(nx), float(j) / float(ny), 0.2f);
+            float u = float(i) / float(nx);
+            float v = float(j) / float(ny);
+
+            Ray r(origin, lowerLeftCorner + u * horizontal + v * vertical);
+
+            vec3 color = Color(r);
             int ir = int(255.99 * color[0]);
             int ig = int(255.99 * color[1]);
             int ib = int(255.99 * color[2]);
