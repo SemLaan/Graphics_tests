f15286c67b0239d41a60180c856f2052cadd7ca0
sem laan <semlaan@dds.nl>
2023-04-08 18:48:01 +0200

    Added a Hitablelist class that inherits from hitable

---
+++ raytracing/raytracing/RayMath/hitablelist.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "hitable.h"
+
+class HitableList : public Hitable {
+public:
+	Hitable** list;
+	int listSize;
+
+	HitableList() {}
+	HitableList(Hitable** l, int n) { list = l; listSize = n; }
+	virtual bool Hit(const Ray& r, float t_min, float t_max, HitRecord& record) const;
+};
+
+bool HitableList::Hit(const Ray& r, float t_min, float t_max, HitRecord& record) const {
+	HitRecord tempRecord;
+	bool hitAnything = false;
+	double closestSoFar = t_max;
+	for (int i = 0; i < listSize; i++) {
+		if (list[i]->Hit(r, t_min, closestSoFar, tempRecord)) {
+			hitAnything = true;
+			closestSoFar = tempRecord.t;
+			record = tempRecord;
+		}
+	}
+	return hitAnything;
+}
