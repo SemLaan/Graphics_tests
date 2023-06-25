12ead34d0ed57be2b2ed79aa1c071832f151dca2
semlaan <semlaan@dds.nl>
2023-06-25 15:13:12 +0200

    Moved some functionality out of the applciation script

--- raytracing/raytracing/src/application.cpp
+++ raytracing/raytracing/src/application.cpp
@@ -12,52 +12,18 @@
 
 int main()
 {
-    unsigned int maxThreads = std::thread::hardware_concurrency();
-    unsigned int numThreads = 100;
-    if (numThreads > maxThreads)
-        numThreads = maxThreads;
+    int width = 600; // width of the image
+    int height = 300; // height of the image
+    int samples = 2; // samples per pixel
 
-    int nx = 600; // width of the image
-    int ny = 300; // height of the image
-    int ns = 200; // samples per pixel
-
-    // Setup array for image data
-    int numChannels = 4; // RGBA
-    
     Camera cam;
-    Hitable* scene = RandomScene(cam, nx, ny);
+    Hitable* scene = RandomScene(cam, width, height);
 
-    unsigned char* imageData = new unsigned char[nx * ny * numChannels];
-    std::thread** threadPool = new std::thread*[numThreads];
-    
-    std::cout << "starting with " << numThreads << " threads" << std::endl;
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    
-    int nyStart = 0;
-    for (unsigned int i = 0; i < numThreads; i++)
-    {
-        int rowsForThisThread = (int)std::floor(ny / (numThreads + 1));
-        unsigned int excessRows = ny % (numThreads + 1);
-        if (i < excessRows)
-            rowsForThisThread++;
-        int nyEnd = nyStart + rowsForThisThread;
-        std::cout << "rows for this thread: " << nyEnd - nyStart << std::endl;
-        threadPool[i] = new std::thread(Render, nx, nyStart, nyEnd, ny, ns, numChannels, &cam, scene, imageData, false);
-        nyStart += rowsForThisThread;
-    }
-
-    std::cout << "rows for this thread: " << ny - nyStart << std::endl;
-    Render(nx, nyStart, ny, ny, ns, numChannels, &cam, scene, imageData, true);
-
-    for (unsigned int i = 0; i < numThreads; i++)
-    {
-        threadPool[i]->join();
-    }
-
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[miliseconds]" << std::endl;
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::seconds> (end - begin).count() << "[seconds]" << std::endl;
+    unsigned char* imageData = RenderToArray(scene, cam, width, height, samples);
 
     // write image to png file
-    stbi_write_png("image.png", nx, ny, numChannels, imageData, nx * numChannels);
+    stbi_write_png("image.png", width, height, TEXTURE_CHANNELS, imageData, width * TEXTURE_CHANNELS);
+
+    delete scene;
+    delete[] imageData;
 }
--- raytracing/raytracing/src/raytracing/raytracingrenderer.h
+++ raytracing/raytracing/src/raytracing/raytracingrenderer.h
@@ -1,19 +1,22 @@
 #pragma once
 #include "raytracing/raytracinginclude.h"
+#include "utils/scenes.h"
 
 
-void Render(int nx, int nyStart, int nyEnd, int ny, int ns, int numChannels, Camera* cam, Hitable* scene, unsigned char* imageData, bool printProgress = false)
+#define TEXTURE_CHANNELS 4
+
+void RenderSubset(int width, int startRow, int endRow, int height, int ns, Camera* cam, Hitable* scene, unsigned char* imageData, bool printProgress = false)
 {
-    for (int j = nyStart; j < nyEnd; j++)
+    for (int j = startRow; j < endRow; j++)
     {
         if (printProgress && j % 2 == 0)
-            std::cout << "progress: " << (float)(j - nyStart) / (nyEnd - nyStart) << std::endl;
-        for (int i = 0; i < nx; i++)
+            std::cout << "progress: " << (float)(j - startRow) / (endRow - startRow) << std::endl;
+        for (int i = 0; i < width; i++)
         {
             Eigen::Vector3f color(0, 0, 0);
             for (int s = 0; s < ns; s++) {
-                float u = float(i + (((double)rand()) / RAND_MAX)) / float(nx);
-                float v = float(j + (((double)rand()) / RAND_MAX)) / float(ny);
+                float u = float(i + (((double)rand()) / RAND_MAX)) / float(width);
+                float v = float(j + (((double)rand()) / RAND_MAX)) / float(height);
 
                 Ray r = cam->GetRay(u, v);
 
@@ -26,11 +29,58 @@ void Render(int nx, int nyStart, int nyEnd, int ny, int ns, int numChannels, Cam
             int ig = int(255.99 * color[1]);
             int ib = int(255.99 * color[2]);
             // Write color data into array
-            int index = ((ny - j - 1) * nx + i) * numChannels;
+            int index = ((height - j - 1) * width + i) * TEXTURE_CHANNELS;
             imageData[index + 0] = unsigned char(ir); // R
             imageData[index + 1] = unsigned char(ig); // G
             imageData[index + 2] = unsigned char(ib); // B
             imageData[index + 3] = unsigned char(255); // A
         }
     }
+}
+
+unsigned char* RenderToArray(Hitable* scene, Camera& cam, unsigned int width, unsigned int height, unsigned int ns)
+{
+    unsigned int maxThreads = std::thread::hardware_concurrency();
+    unsigned int numThreads = 100;
+    if (numThreads > maxThreads)
+        numThreads = maxThreads;
+
+    unsigned char* imageData = new unsigned char[width * height * TEXTURE_CHANNELS];
+    std::thread** threadPool = new std::thread*[numThreads];
+
+    std::cout << "starting with " << numThreads << " threads" << std::endl;
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+    int startRow = 0;
+    for (unsigned int i = 0; i < numThreads; i++)
+    {
+        int rowsForThisThread = (int)std::floor(height / (numThreads + 1));
+        unsigned int excessRows = height % (numThreads + 1);
+        if (i < excessRows)
+            rowsForThisThread++;
+        int endRow = startRow + rowsForThisThread;
+        std::cout << "rows for this thread: " << endRow - startRow << std::endl;
+        threadPool[i] = new std::thread(RenderSubset, width, startRow, endRow, height, ns, &cam, scene, imageData, false);
+        startRow += rowsForThisThread;
+    }
+
+    std::cout << "rows for this thread: " << height - startRow << std::endl;
+    RenderSubset(width, startRow, height, height, ns, &cam, scene, imageData, true);
+
+    for (unsigned int i = 0; i < numThreads; i++)
+    {
+        threadPool[i]->join();
+    }
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[miliseconds]" << std::endl;
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::seconds> (end - begin).count() << "[seconds]" << std::endl;
+
+    for (int i = 0; i < numThreads; i++)
+    {
+        delete threadPool[i];
+    }
+    delete[] threadPool;
+
+    return imageData;
 }
